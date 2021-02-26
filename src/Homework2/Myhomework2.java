a6da1e70ec22ae1f75409c49c2617ce157755e29
Jerianna Gottschalk <jeriannagottschalk@jeriannas-mbp.myfiosgateway.com>
2021-02-26 12:49:06 -0500

    homework 2

---
+++ src/Homework2/Myhomework2.java
@@ -0,0 +1,62 @@
+package Homework2;
+
+
+public class Myhomework2 {
+    public static void main(String[] args) {
+
+        byte numOfHoursInDays = 24;
+
+
+        System.out.println( "there are" + numOfHoursInDays);
+
+
+        short daysInYear = 365;
+        short currentYear = 2021;
+
+        System.out.println("there are" + daysInYear + "in year" +currentYear);
+
+        int numOfEmployees = 200000;
+
+        System.out.println( "there are" + numOfEmployees + "employees in a company");
+
+        long numOfPeopleInCountry = 90000000l;
+
+
+        System.out.println("there are"+ numOfPeopleInCountry+ "in a country");
+
+        float interestRate = 2.8f;
+
+        System.out.println("interest rate" + interestRate);
+
+        boolean isSunRiseOnWest = false;
+
+        System.out.println("does the sun rise in the west?" + isSunRiseOnWest);
+
+        double  bankAcc= 20000;
+
+        System.out.println("balance in bank account"+ bankAcc);
+
+        char FirstInt = 'j';
+        char lastInt = 'G';
+
+        System.out.println("first name"+ FirstInt + "last name" + lastInt );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
